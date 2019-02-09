040e49aa12cbe210ff8e5c7b94e96218e24c9840
Rauan <rauan.shyrynbekov@gmail.com>
2019-02-09 12:34:16 +0600

    my first commit

---
+++ Lab2.3/Lab2.3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace W3
+{
+    class Program
+    {
+        static string Spaces(int a)                       //for the spaces
+        {
+            string s = "      ";
+            string q = "";
+            for (int i = 0; i < a; i++)
+            {
+                q = q + s;
+            }
+            return q;
+        }
+        static void Paths(DirectoryInfo dir, int a)   //recursion for the open the folders and write the name of them
+        {
+            FileInfo[] files = dir.GetFiles();
+            
+            DirectoryInfo[] dires = dir.GetDirectories();
+            foreach (FileInfo file in files)          //write files name with some space on the front
+            {
+               
+                Console.Write(Spaces(a));
+                Console.WriteLine(file.Name);
+
+            }
+            foreach (DirectoryInfo dire in dires)       //write folders name with some space on the front
+            {
+                
+                Console.Write(Spaces(a));
+                Console.WriteLine(dire.Name);
+
+                Paths(dire, a + 1);
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"D:\");
+            Paths(dir, 0);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab2.1/lab2.1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab21
+{
+    class Program
+    {
+        static bool Pako(string n)        //function which check the words by polyndrome principle
+        {
+            bool t = true;
+            for (int i = 0; i < n.Length / 2; i++)
+            {
+                if (n[i] != n[n.Length - 1 - i])//This code check by the method which get last symbol of the words and 
+                { return false; }
+            }
+            return t;
+
+        }
+        static void Main(string[] args)
+        {
+            string words = System.IO.File.ReadAllText(@"C:\Users\HP\Desktop\C# test\lab2.1\1.txt");//reads the text in file in given by directory
+
+            if (Pako(words))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            { Console.WriteLine("No"); }
+            Console.ReadKey();
+        }
+    }
+}
